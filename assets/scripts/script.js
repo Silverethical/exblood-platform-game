31d70efe85f11d31d32397db2324612d7b743928
Saber Heydari <silverethical@gmail.com>
2023-01-12 13:35:26 +0330

    feat: background sprite

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,10 @@
 const canvas = document.querySelector("canvas"),
   c = canvas.getContext("2d"),
   gravity = 0.5,
+  scale = {
+    x: 4,
+    y: 4,
+  },
   keys = {
     a: { pressed: false },
     d: { pressed: false },
@@ -13,8 +17,26 @@ const canvas = document.querySelector("canvas"),
 canvas.width = 1024;
 canvas.height = 576;
 
+/** Sprite class */
+class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position;
+    this.image = new Image();
+    this.image.src = imageSrc;
+  }
+
+  draw() {
+    if (!this.image) return;
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
 /** Player class */
-class Player {
+export class Player {
   constructor(position) {
     this.position = position;
     this.velocity = {
@@ -48,8 +70,14 @@ class Player {
   }
 }
 
-/** declare a new player */
+/** create a new player */
 const player1 = new Player({ x: 100, y: 100 });
+
+/** create a background */
+const background = new Sprite({
+  position: { x: 0, y: 0 },
+  imageSrc: "./assets/images/backgrounds/background.png",
+});
 
 /** create animate function */
 function animate() {
@@ -59,6 +87,15 @@ function animate() {
   /** clear canvas by drawing a gray rectangle on the canvas */
   c.fillStyle = "gray ";
   c.fillRect(0, 0, canvas.width, canvas.height);
+
+  c.save();
+  /** scale to 4 (just for background) */
+  c.scale(scale.x, scale.y);
+  /** move to the bottom of the image */
+  c.translate(0, -background.image.height + canvas.height / scale.y);
+  /** update background */
+  background.update();
+  c.restore();
 
   /** update player position */
   player1.update();
